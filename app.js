bcfbafbad81a28a0f0c07bae9fcc6a0bb3cee1d2
tulsagospelassembly <tulsagospelassembly@gmail.com>
2020-06-24 18:45:02 -0500

    changed styling and layout

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
 
-    $(".burger-icon").on("click", function() {
-        if ($(".main-nav").hasClass("mobile-hide")) {
-            $(".main-nav").removeClass("mobile-hide");
-            $(".main-nav").addClass("mobile-show");
-        } else {
-            $(".main-nav").removeClass("mobile-show");
-            $(".main-nav") .addClass("mobile-hide");      
-        }
+    $(".burger-icon").click(function(){
+        $(this).toggleClass("fa-times");
+        $(".nav-items").toggleClass("active");
     });
+
+    // $(".burger-icon").on("click", function() {
+    //     if ($(".main-nav").hasClass("mobile-hide")) {
+    //         $(".main-nav").removeClass("mobile-hide");
+    //         $(".main-nav").addClass("mobile-show");
+    //     } else {
+    //         $(".main-nav").removeClass("mobile-show");
+    //         $(".main-nav") .addClass("mobile-hide");      
+    //     }
+    // });
 });
 
